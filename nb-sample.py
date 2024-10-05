b939878519ceabb59478bf39b6b0fe9f81cbef88
Sharpaste <n22126@gmail.com>
2024-10-05 20:38:38 +0800

    feat(nb): added bernoulli_nb

--- nb-sample.py
+++ nb-sample.py
@@ -5,6 +5,7 @@ import sys
 import pandas as pd
 import numpy as np
 
+from tqdm import tqdm
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 
@@ -16,63 +17,37 @@ if TEST:
     def gen_custum_train_data() -> (
         tuple[pd.DataFrame, pd.DataFrame, np.ndarray, np.ndarray]
     ):
-        """
-        Generate custom training and test data.
-
-        Returns
-        -------
-        X_train : np.ndarray
-            The training data.
-        X_test : np.ndarray
-            The test data.
-        y_train : np.ndarray
-            The training labels.
-        y_test : np.ndarray
-            The test labels.
-        """
         test_ratio = 0.2
         df = pd.read_csv("./text.txt", sep="\t", header=None, encoding="utf-8")
         count_vect = CountVectorizer(token_pattern=r"(?u)\b\w+\b")
         X = count_vect.fit_transform(df[0])
         print(count_vect.__dict__["vocabulary_"])
+        print("vocabulary table")
+        print("------------------")
         y = df[1]
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=test_ratio, shuffle=False
         )
 
-        # DEBUG
         print(X_train.shape, X_test.shape, len(y_train), len(y_test))
 
         return X_train, X_test, y_train, y_test
 
 
 def gen_train_test_data() -> tuple[pd.DataFrame, pd.DataFrame, np.ndarray, np.ndarray]:
-    """
-    Generate training and test data from the Yelp labelled dataset.
-
-    Returns
-    -------
-    X_train : np.ndarray
-        The training data.
-    X_test : np.ndarray
-        The test data.
-    y_train : np.ndarray
-        The training labels.
-    y_test : np.ndarray
-        The test labels.
-    """
     test_ratio = 0.3
     df = pd.read_csv("./yelp_labelled.txt", sep="\t", header=None, encoding="utf-8")
     count_vect = CountVectorizer()
     X = count_vect.fit_transform(df[0])
     print(count_vect.__dict__["vocabulary_"])
+    print("vocabulary table")
+    print("------------------")
     y = df[1]
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=test_ratio, random_state=0
     )
 
-    # DEBUG
-    print(X_train.shape, X_test.shape, len(y_train), len(y_test))
+    print(f"train shape: {X_train.shape}, test shape: {X_test.shape}")
 
     return X_train, X_test, y_train, y_test
 
@@ -80,21 +55,7 @@ def gen_train_test_data() -> tuple[pd.DataFrame, pd.DataFrame, np.ndarray, np.nd
 def multinomial_nb(
     X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.Series, y_test: pd.Series
 ) -> None:
-    """
-    Train a Multinomial Naive Bayes model given the training data and report the
-    accuracy on both training and test data.
-
-    Parameters
-    ----------
-    X_train : pd.DataFrame
-        The training data.
-    X_test : pd.DataFrame
-        The test data.
-    y_train : pd.Series
-        The labels for the training data.
-    y_test : pd.Series
-        The labels for the test data.
-    """
+    print("\nMultinomial NB:")
 
     pos_num = y_train.value_counts()[1]
     neg_num = y_train.value_counts()[0]
@@ -109,7 +70,10 @@ def multinomial_nb(
     }
 
     for given_y in ["positive", "negative"]:
-        for x_i in range(conditional_probs[given_y].shape[0]):
+        for x_i in tqdm(
+            range(conditional_probs[given_y].shape[0]),
+            desc=f"calculating {given_y} conditional probs",
+        ):
             if given_y == "positive":
                 repr_y = 1
             else:
@@ -153,9 +117,62 @@ def multinomial_nb(
 
 
 def bernoulli_nb(X_train, X_test, y_train, y_test):
-    # TODO: fill this function
-    # train by X_train and y_train
-    # report the predicting accuracy for both the training and the test data
+    print("\nBernoulli NB:")
+
+    pos_num = y_train.value_counts()[1]
+    neg_num = y_train.value_counts()[0]
+    prior_prob = {
+        "positive": pos_num / (pos_num + neg_num),
+        "negative": neg_num / (pos_num + neg_num),
+    }
+
+    conditional_probs = {
+        "positive": np.zeros(X_train.shape[1]),
+        "negative": np.zeros(X_train.shape[1]),
+    }
+
+    for given_y in ["positive", "negative"]:
+        for x_i in tqdm(
+            range(conditional_probs[given_y].shape[0]),
+            desc=f"calculating {given_y} conditional probs",
+        ):
+            if given_y == "positive":
+                repr_y = 1
+            else:
+                repr_y = 0
+            if SMOOTH:
+                conditional_probs[given_y][x_i] = (
+                    np.count_nonzero(X_train[y_train == repr_y][:, x_i].data) + 1
+                ) / (X_train[y_train == repr_y][:, x_i].shape[0] + 2)
+            else:
+                conditional_probs[given_y][x_i] = (
+                    np.count_nonzero(X_train[y_train == repr_y][:, x_i].data)
+                    / X_train[y_train == repr_y][:, x_i].shape[0]
+                )
+
+    for phase in ["train", "test"]:
+        if phase == "train":
+            X = X_train
+            y = y_train
+        else:
+            X = X_test
+            y = y_test
+        predicted = []
+        for test_case in X:
+            test_case: pd.DataFrame
+            highest_prob = 0
+            highest_prob_y = None
+            for pos_neg in ["positive", "negative"]:
+                prob = prior_prob[pos_neg]
+                for x_now in range(test_case.indices.shape[0]):
+                    feature = test_case.indices[x_now]
+                    prob *= conditional_probs[pos_neg][feature]
+                if prob > highest_prob:
+                    highest_prob = prob
+                    highest_prob_y = pos_neg
+            predicted.append(1 if highest_prob_y == "positive" else 0)
+        predicted = np.array(predicted)
+        print(f"{phase} accuracy: {sum(predicted == y.values) / len(y.values) * 100}%")
     pass
 
 
